8c13d4aea37c8f72e230e015b41876af7c8e188b
Mendy2050 <876446372@qq.com>
2023-08-22 11:11:53 +0100

    CD workflow with new controller

---
+++ backend/src/main/java/com/mendy/PingPongController.java
@@ -0,0 +1,21 @@
+package com.mendy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Mendy
+ * @create 2023-08-22 11:07
+ */
+
+@RestController
+public class PingPongController {
+
+    record PingPong(String result){}
+
+    @GetMapping("/ping")
+    public PingPong getPingPong(){
+        return new PingPong("Pong");
+    }
+
+}
