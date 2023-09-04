22a3e443e115e59bdb341500e37e8a987164506f
Mendy2050 <876446372@qq.com>
2023-09-04 14:26:42 +0100

    change to Deploy to Elastic Beanstalk 09.04

---
+++ frontend/React/src/components/CreateCustomerDrawer.jsx
@@ -0,0 +1,65 @@
+import {
+    Button,
+    Drawer,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    DrawerHeader,
+    DrawerBody,
+    DrawerFooter,
+    useDisclosure
+} from "@chakra-ui/react";
+import CreateCustomerForm from "./CreateCustomerForm.jsx";
+
+
+const AddIcon = () => "+";
+const CloseIcon = () => "×";
+
+const CreateCustomerDrawer = ({fetchCustomers} ) => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
+    return <>
+
+
+        <Button
+            leftIcon={<AddIcon/>}
+            colorScheme={"teal"}
+            onClick={onOpen}
+        >
+            Create customer
+        </Button>
+
+        <Drawer isOpen={isOpen} onClose={onClose}  size={"xl"}>
+            <DrawerOverlay />
+            <DrawerContent>
+                <DrawerCloseButton />
+                <DrawerHeader>Create New Customer</DrawerHeader>
+
+                <DrawerBody>
+
+                    <CreateCustomerForm
+                        fetchCustomers = { fetchCustomers }
+                    />
+
+                </DrawerBody>
+
+                <DrawerFooter>
+                    <Button
+                        leftIcon={<CloseIcon/>}
+                        colorScheme={"teal"}
+                        onClick={onClose}
+                    >
+                    Close
+                    </Button>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+
+
+        </>
+
+}
+
+export default CreateCustomerDrawer;
+
+
